bb6fb3d6684c6a46a26923fef36548374b5c7d78
Cassio Maciel Lemos <cassio.lemos@petrobras.com.br>
2024-03-25 19:03:10 -0300

    All new function pace_marathon changed de tests functions

--- src/vdot_calculator/__init__.py
+++ src/vdot_calculator/__init__.py
@@ -2,4 +2,5 @@ from .func_module import (
     vdot_from_distance_and_pace,
     vdot_from_time_and_distance,
     vdot_from_time_and_pace,
+    pace_marathon,
 )
--- src/vdot_calculator/func_module.py
+++ src/vdot_calculator/func_module.py
@@ -173,3 +173,47 @@ def check_time(value) -> datetime.time:
     if isinstance(value, datetime.time):
         return value
     raise TypeError('The input should be type datetime.time')
+
+
+def pace_marathon(v_dot: float) -> datetime.time:
+    """
+    Calculate the running pace corresponding to the given VDOT for a marathon.
+
+    Args:
+    - v_dot (float): The VDOT value for which to calculate the running pace.
+
+    Returns:
+    - datetime.time: The calculated running pace represented as a time object.
+    """
+    continue_criteria = True
+    index = 0.0
+    t_min = 50.0
+    v_dot_min = direct(t_min, 42195)
+    t_max = 360.0
+    t_med = (t_min + t_max) / 2
+    v_dot_max = direct(t_max, 42195)
+    while continue_criteria:
+        index += 1
+        t_med = (t_min + t_max) / 2
+        v_dot_med = direct(t_med, 42195)
+        error = v_dot_med - v_dot
+        if error / (v_dot_max-v_dot) > 0:
+            t_max = t_med
+            v_dot_max = v_dot_med
+        elif error / (v_dot_min-v_dot) > 0:
+            t_min = t_med
+            v_dot_min = v_dot_med
+        if abs(error) < 0.01:
+            continue_criteria = False
+        if index > 400:
+            print('WARNING: doesn\'t solve')
+            continue_criteria = False
+
+    pace_minutes = t_med/42.195
+    minutes = int(math.floor(pace_minutes))
+    seconds = int(math.floor((pace_minutes-minutes)*60))
+    output = datetime.time(
+                       minute=minutes,
+                       second=seconds)
+
+    return output
